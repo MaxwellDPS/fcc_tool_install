c4e9d78d89ac97ee4b2c2fcd4173eb71abb18a10
Maxwell Watermolen <mwatermolen@maxwelldps.com>
2018-11-02 23:46:26 -0500

    add file

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+BASEDIR=$PWD
+DIR=$BASEDIR"/fcc"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+echo "Installing php"
+apt install php -y 
+
+echo "Git fcc tool"
+git clone https://github.com/FCC/mmba_JSON_bulkimporter.git
+
+mv mmba_JSON_bulkimporter fcc
+
+sed -i 's|DEFINE."BASE_PATH",".*".;|DEFINE\("BASE_PATH","'"$DIR"'"\);|g' fcc/functions.php
+
+sudo chmod -R 777 fcc
+
+rm fcc/data/*
