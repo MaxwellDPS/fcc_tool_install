159c17d5a5e4c39d4c4b5db38cbd2b98d363e9ff
Maxwell Watermolen <mwatermolen@maxwelldps.com>
2018-11-03 02:53:18 -0500

    Removed full php install

--- install.sh
+++ install.sh
@@ -13,9 +13,7 @@ apt install php*-cli -y
 git clone https://github.com/FCC/mmba_JSON_bulkimporter.git
 
 mv mmba_JSON_bulkimporter fcc
-
 sed -i 's|DEFINE."BASE_PATH",".*".;|DEFINE\("BASE_PATH","'"$DIR"'"\);|g' fcc/functions.php
 
 chown -R $currentuser:$currentuser fcc
-
 rm fcc/data/*
